a844a8dea138253a3d1a5fe3e74a2bf0285ccb09
Sam Ryu <rockhead51@naver.com>
2021-10-24 04:16:01 +0900

    Initial commit

---
+++ programmers_77484.py
@@ -0,0 +1,24 @@
+
+def solution(lottos, win_nums):
+
+
+    answer = []
+    unknownCnt = 0
+    matchCnt = 0
+
+    for i in range(len(lottos)):
+        if lottos[i] == 0:
+            unknownCnt += 1
+        for j in range(len(win_nums)):
+            if win_nums[j] == lottos[i]:
+                matchCnt += 1
+
+    best = int(min(7 - (unknownCnt + matchCnt), 6))
+    worst = int(min(7 - matchCnt, 6))
+
+    answer.append(best)
+    answer.append(worst)
+
+    return answer
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
